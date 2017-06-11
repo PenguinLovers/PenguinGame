e7f7d2c35644c9365b809190d6566aac3fb7fc95
keyakiman <keyakiman119@gmail.com>
2017-06-11 22:48:40 +0900

    ダイスの判定部分追加

--- Sugoroku/Assets/Scripts/DiceController.cs
+++ Sugoroku/Assets/Scripts/DiceController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceController : MonoBehaviour {
 
     public float power = 20.0f;
     public float vy = 500.0f;
+    public Text DiceText;
+    private int diceScore = 0;
 
     private Rigidbody dice;
 
 	// Use this for initialization
 	void Start () {
+        DiceText.text = "Dice: X";
         dice = GetComponent<Rigidbody>();
 
         // 原点から-5～5ずらした位置に向かって投げる
@@ -27,6 +31,49 @@ public class DiceController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 check_1 = transform.TransformDirection(Vector3.forward);
+        Vector3 check_5 = transform.TransformDirection(Vector3.right);
+        Vector3 check_4 = transform.TransformDirection(Vector3.left);
+        int result = 0;
+
+        if (Mathf.Abs(Mathf.Round(check_1.y)) != 1)
+        {
+            if (Mathf.Abs(Mathf.Round(check_4.y)) != 1)
+            {
+                if (Mathf.Round(check_5.y) == 1)
+                {
+                    result = 5;
+                }
+                else
+                {
+                    result = 2;
+                }
+            }
+            else
+            {
+                if (Mathf.Round(check_4.y) == 1)
+                {
+                    result = 4;
+                }
+                else
+                {
+                    result = 3;
+                }
+            }
+        }
+        else
+        {
+            if (Mathf.Round(check_1.y) == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 6;
+            }
+        }
+
+        DiceText.text = "Dice:" + result;
+
 	}
 }
