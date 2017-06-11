e8f10e0530935b197c97d0675591bb40fba450fb
colque <yraboc.hm12@gmail.com>
2017-06-11 23:29:47 +0900

    振り直し関数を追加

--- Sugoroku/Assets/Scripts/DiceController.cs
+++ Sugoroku/Assets/Scripts/DiceController.cs
@@ -12,25 +12,42 @@ public class DiceController : MonoBehaviour {
 
     private Rigidbody dice;
 
-	// Use this for initialization
-	void Start () {
+    private bool b_keyUp;
+
+    // Use this for initialization
+    void Start () {
         DiceText.text = "Dice: X";
         dice = GetComponent<Rigidbody>();
-
-        // 原点から-5～5ずらした位置に向かって投げる
-        float directionX = Random.Range(-5.0f, 5.0f);
-        float directionZ = Random.Range(-5.0f, 5.0f);
-        Vector3 v = new Vector3(directionX, 0.0f, directionZ) - dice.transform.position;
-        v.y = vy / power; // yはあとで*powerするので固定値になる
-
-        // 力を加える位置
-        Vector3 forcePosition = dice.transform.position - new Vector3(0.5f, 0.0f, -0.5f);
-
-        dice.AddForceAtPosition(v * power, forcePosition);
+        DiceRoll();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (dice.velocity.magnitude == 0.0f)
+        {
+            if (Application.platform == RuntimePlatform.Android
+              || Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    if (touch.phase == TouchPhase.Ended)
+                    {
+                        // タッチ終了
+                        b_keyUp = true;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    b_keyUp = true;
+                }
+            }
+        }
+
+        // 以下、出目判定
         Vector3 check_1 = transform.TransformDirection(Vector3.forward);
         Vector3 check_3 = transform.TransformDirection(Vector3.right);
         Vector3 check_2 = transform.TransformDirection(Vector3.up);
@@ -76,4 +93,34 @@ public class DiceController : MonoBehaviour {
         DiceText.text = "Dice:" + result;
 
 	}
+
+    void FixedUpdate()
+    {
+        if( b_keyUp )
+        {
+            InitDicePosition();
+            DiceRoll();
+            b_keyUp = false;
+        }
+    }
+
+    void InitDicePosition()
+    {
+        dice.transform.position = new Vector3(6.0f, 6.0f, 6.0f);
+        dice.transform.rotation = Quaternion.Euler(-55.0f, 108.0f, -135.0f);
+    }
+
+    void DiceRoll()
+    {
+        // 原点から-5～5ずらした位置に向かって投げる
+        float directionX = Random.Range(-5.0f, 5.0f);
+        float directionZ = Random.Range(-5.0f, 5.0f);
+        Vector3 v = new Vector3(directionX, 0.0f, directionZ) - dice.transform.position;
+        v.y = vy / power; // yはあとで*powerするので固定値になる
+
+        // 力を加える位置
+        Vector3 forcePosition = dice.transform.position - new Vector3(0.5f, 0.0f, -0.5f);
+
+        dice.AddForceAtPosition(v * power, forcePosition);
+    }
 }
