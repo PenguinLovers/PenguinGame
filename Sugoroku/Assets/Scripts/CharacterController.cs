4d747fef6e64cc2c8c88c9478a3cabfbf2ce4e3c
colque <yraboc.hm12@gmail.com>
2017-06-14 19:01:33 +0900

    盤面シーン追加。コマ移動まで。

---
+++ Sugoroku/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    private int nowMass = 0;
+    GameObject nowMassObj;
+    GameObject childMassObj;
+
+    Rigidbody character;
+
+    private bool b_keyUp = false;
+    private bool isMoving = false;
+
+    // Use this for initialization
+    void Start () {
+        character = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            b_keyUp = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(!isMoving && b_keyUp)
+        {
+            StartCoroutine("GoToNext");
+            isMoving = true;
+            b_keyUp = false;
+        }
+    }
+
+    IEnumerator GoToNext()
+    {
+        if (nowMassObj == null || childMassObj == null)
+        {
+            GetMassInfo();
+        }
+
+        // 次のマスの位置
+        Vector3 dest = childMassObj.transform.position;
+        Debug.Log(character.transform.position + "," + dest);
+        dest.y = character.transform.position.y;    // キャラクターの高さは変えない
+
+        while ( Mathf.Abs(character.transform.position.x - dest.x) > 0.05f
+            || Mathf.Abs(character.transform.position.z - dest.z) > 0.05f)
+        {
+            character.transform.position += (dest - character.transform.position) / 20.0f;
+            yield return null;
+        }
+
+        // 目的地に到着したと思うのでマスの情報を取得しなおしておく
+        UpdateNowMass();
+        GetMassInfo();
+        isMoving = false;
+
+        StopCoroutine("GoToNext");
+    }
+
+    void GetMassInfo()
+    {
+        // 現在のマスを取得
+        string massName = "Mass" + nowMass;
+        nowMassObj = GameObject.Find(massName);
+        MassController mc = nowMassObj.GetComponent<MassController>();
+
+        // 現在のマスから次のマス（子）を取得
+        string destMassName = "Mass" + mc.GetChild();
+        Debug.Log(massName + "'s child is " + destMassName);
+        childMassObj = GameObject.Find(destMassName);
+    }
+
+    void UpdateNowMass()
+    {
+        MassController mc = nowMassObj.GetComponent<MassController>();
+        nowMass = mc.GetChild();
+    }
+}
---
+++ Sugoroku/Assets/Scripts/MassController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassController : MonoBehaviour {
+    [SerializeField]
+    private int childMass;
+    //取得用関数
+    public int GetChild()
+    {
+        return childMass;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
